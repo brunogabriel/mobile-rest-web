d0bc6362d81d90ed7e8fe969ecc0f67dff350ef7
brunogabriel <bruno.gsantos89@gmail.com>
2016-07-18 21:26:42 -0300

    Restarting development, changing database

--- mrestweb/webapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from models import Device
-
-class DeviceAdmin(admin.ModelAdmin):
-	list_display = ('user', 'platform', 'device_identifier',)
-
-# registers
-admin.site.register(Device, DeviceAdmin)
--- mrestweb/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.conf.urls import include
-from . import views
-
-urlpatterns = [
-	url(r'^user/login$', views.do_login),
-	url(r'^user/createaccount$', views.do_create_account),
-]
